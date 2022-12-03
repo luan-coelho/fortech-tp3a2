50898899b0b7d402362571ee44b64dcd26590010
Luan Coelho de Souza <luancoelho@unitins.br>
2022-12-03 15:08:37 -0300

    CRUD de Marca

---
+++ FortechTP3A2/Controllers/ModeloController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FortechTP3A2.Data;
+using FortechTP3A2.Models;
+
+namespace FortechTP3A2.Controllers
+{
+    public class ModeloController : Controller
+    {
+        private readonly FortechContext _context;
+
+        public ModeloController(FortechContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modelo
+        public async Task<IActionResult> Index()
+        {
+            var fortechContext = _context.Modelo.Include(m => m.Marca);
+            return View(await fortechContext.ToListAsync());
+        }
+
+        // GET: Modelo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Modelo == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelo
+                .Include(m => m.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            return View(modelo);
+        }
+
+        // GET: Modelo/Create
+        public IActionResult Create()
+        {
+            ViewData["MarcaId"] = new SelectList(_context.Marca, "Id", "Descricao");
+            return View();
+        }
+
+        // POST: Modelo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descricao,MarcaId")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(modelo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marca, "Id", "Descricao", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // GET: Modelo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Modelo == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelo.FindAsync(id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marca, "Id", "Descricao", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // POST: Modelo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,MarcaId")] Modelo modelo)
+        {
+            if (id != modelo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(modelo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModeloExists(modelo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marca, "Id", "Descricao", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // GET: Modelo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Modelo == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelo
+                .Include(m => m.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            return View(modelo);
+        }
+
+        // POST: Modelo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Modelo == null)
+            {
+                return Problem("Entity set 'FortechContext.Modelo'  is null.");
+            }
+            var modelo = await _context.Modelo.FindAsync(id);
+            if (modelo != null)
+            {
+                _context.Modelo.Remove(modelo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModeloExists(int id)
+        {
+          return _context.Modelo.Any(e => e.Id == id);
+        }
+    }
+}
--- FortechTP3A2/Models/Marca.cs
+++ FortechTP3A2/Models/Marca.cs
@@ -1,10 +1,18 @@
-﻿namespace FortechTP3A2.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FortechTP3A2.Models
 {
     public class Marca
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe a Descrição")]
+        [DisplayName("Descrição")]
+        [MaxLength(50, ErrorMessage = "Informe no máximo 50 caracteres")]
+        [MinLength(0)]
         public string Descricao { get; set; }
 
-        public IList<Modelo> Modelos { get; set; }
+        public IList<Modelo>? Modelos { get; set; }
    }
 }
